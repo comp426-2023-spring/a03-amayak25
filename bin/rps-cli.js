536afa2c13d55bf4651998d107f2aea1e743904f
Kirsten <amayak@email.unc.edu>
2023-02-23 10:37:41 -0500

    Update rps-cli.js

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -2,7 +2,7 @@
 #!/usr/bin/env node 
 import minimist from 'minimist';
 import fetch from 'node-fetch';
-//import { rps } from "/lib/rpsls.js"
+import { rps } from "/lib/rpsls.js"
 
 const args = minimist(process.argv.slice(2));
 
