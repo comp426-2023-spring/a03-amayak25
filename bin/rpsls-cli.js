87e8bf827731ad9aa062f75216ec4bdd52600dbc
Kirsten <amayak@email.unc.edu>
2023-02-23 20:21:33 -0500

    Update rpsls-cli.js
    I had a copy and paste error

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -5,27 +5,35 @@ import { rpsls } from "../lib/rpsls.js";
 
 const args = minimist(process.argv.slice(2));
 
+function help()
 {
-  console.log('Usage: node rps.js [SHOT]\n');
-  console.log('Play Rock Paper Scissors (RPS)\n');
+  console.log('Usage: node-rpsls [SHOT]\n');
+  console.log('Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!\n');
   console.log('Options:');
   console.log('-h, --help  display this help message and exit');
   console.log('-r, --rules display the rules and exit\n');
   console.log('Examples:');
-  console.log('node rps.js                  Return JSON with single player RPS result.');
+  console.log('node-rpsls                  Return JSON with single player RPSLS result.');
   console.log('                             e.g. {"player":"rock"}');
-  console.log('node rps.js rock             Return JSON with results for RPS played against a simulated opponent.');
-  console.log('                             e.g {"player":"rock","opponent":"scissors","result":"win"}');
+  console.log('node-rpsls rock             Return JSON with results for RPSLS played against a simulated opponent.');
+  console.log('                             e.g {"player":"rock","opponent":"Spock","result":"lose"}');
   process.exit(0);
 }
 
 function rules() 
-
-{
-  console.log('  Rules for Rock Paper Scissors:');
-  console.log('- Scissors CUTS Paper');
-  console.log('- Paper COVERS Rock');
-  console.log('- Rock CRUSHES Scissors')
+  {
+    console.log('Rules for Lizard-Spock Expansion of Rock Paper Scissors:');
+    console.log('- Scissors CUTS Paper');
+    console.log('- Paper COVERS Rock');
+    console.log('- Rock CRUSHES Lizard');
+    console.log('- Lizard POISONS Spock');
+    console.log('- Spock SMASHES Scissors');
+    console.log('- Scissors DECAPITATES Lizard');
+    console.log('- Lizard EATS Paper');
+    console.log('- Paper DISPROVES Spock');
+    console.log('- Spock VAPORIZES Rock');
+    console.log('- Rock CRUSHES Scissors');
+    process.exit(0);
 }
 if (args[0] === '-h' || args[0] === '--help') 
 {
@@ -48,4 +56,4 @@ else
     console.log(JSON.stringify(result));
     
     } 
-    }
+  }
