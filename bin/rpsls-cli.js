9987251b829cffadbb30189bec77963a91ad8eb1
Kirsten <amayak@email.unc.edu>
2023-02-28 20:29:38 -0500

    Update rpsls-cli.js

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -53,7 +53,7 @@ else {
     console.log(JSON.stringify(result));
   } catch (error) {
     console.error(`${args._[0]} is out of range.`);
-   help();
-    rules();
+   console.log(help());
+    console.log(rules());
   }
 }
